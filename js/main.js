53cdf3bb45d41382fd43efb000584f5da0467fc3
John Skender <jnskender@gmail.com>
2017-01-26 09:29:05 -0600

    Add dynamic video links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+  var linkArray = ["https://www.youtube.com/embed/zjoE-KibMnc",
+  "https://www.youtube.com/embed/VPE8P3we2hE",
+  "https://www.youtube.com/embed/93nQVtZeXwk",
+  "https://youtu.be/iiIGerCU5Rg",
+  "https://youtu.be/Tt4XAZzg0pw",
+  "https://youtu.be/LoJvqUAmvck",
+  "https://youtu.be/KGbQfiprj5I",
+  "https://youtu.be/goHVRvXJbOA",
+  "https://youtu.be/v3CriQBWZPo",
+  "https://youtu.be/RWRgZMSNYM0"];
 
+
+
+  $("iframe").attr("src", linkArray[0]);
+
+
+
+});

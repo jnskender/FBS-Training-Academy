2e898b4d90036cf71f58c745a1e9e663047fd74a
John Skender <jnskender@gmail.com>
2017-01-27 15:31:40 -0600

    Alpha 1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-$(document).ready(function(){
-  var linkArray = ["https://www.youtube.com/embed/zjoE-KibMnc",
-  "https://www.youtube.com/embed/VPE8P3we2hE",
-  "https://www.youtube.com/embed/93nQVtZeXwk",
-  "https://youtu.be/iiIGerCU5Rg",
-  "https://youtu.be/Tt4XAZzg0pw",
-  "https://youtu.be/LoJvqUAmvck",
-  "https://youtu.be/KGbQfiprj5I",
-  "https://youtu.be/goHVRvXJbOA",
-  "https://youtu.be/v3CriQBWZPo",
-  "https://youtu.be/RWRgZMSNYM0"];
+$(document).ready(function() {
+    var trainingLinks = ["https://www.youtube.com/embed/zjoE-KibMnc",
+        "https://www.youtube.com/embed/VPE8P3we2hE",
+        "https://www.youtube.com/embed/93nQVtZeXwk",
+        "https://www.youtube.com/embed/iiIGerCU5Rg",
+        "https://www.youtube.com/embed/Tt4XAZzg0pw",
+        "https://www.youtube.com/embed/LoJvqUAmvck",
+        "https://www.youtube.com/embed/KGbQfiprj5I",
+        "https://www.youtube.com/embed/goHVRvXJbOA",
+        "https://www.youtube.com/embed/v3CriQBWZPo",
+        "https://www.youtube.com/embed/RWRgZMSNYM0"
+    ];
 
-
-
-  $("iframe").attr("src", linkArray[0]);
-
-
-
-});
+    $('.thumbnail').each(function() {
+        $(this).click(function() {
+            var idVal = parseInt($(this).attr("id")) - 1;
+            $("#youtubeVideo").attr("src", trainingLinks[idVal]);
+            $(".hidden").toggleClass("hidden");
+            $("#youtubeVideo").contentWindow.focus();
+        }); // end click
+    }); // end each
+}); // end ready
